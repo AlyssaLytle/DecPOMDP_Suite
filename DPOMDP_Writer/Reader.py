dd768b70442aa54c46db75e5e12d737fba8789d5
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-05-25 09:23:26 -0400

    Added reading methods to read trees from files

---
+++ DPOMDP_Writer/Reader.py
@@ -0,0 +1,49 @@
+filename = "testpolicy"
+
+import sys
+sys.path.append("..")
+from ArrayTree import ArrayTree
+
+def get_node_and_edge(line):
+    [edge, node] = line.split("-->")
+    if len(edge) > 3:
+        edge = edge[1:-2]
+    else:
+        edge = ""
+    node = node[1:-1]
+    node = node.split("-")
+    return [edge,node]
+
+def get_trees(filename, agent0_branch_size, agent1_branch_size):   
+    f = open(filename, "r")
+    #skip first 3 lines
+    f.readline()
+    f.readline()
+    f.readline()
+    flag = True
+    edges = []
+    nodes = []
+    while(flag):
+        l = f.readline()
+        if l[0] == "(":
+            [edge, node] = get_node_and_edge(l)
+            nodes.append(node)
+            edges.append(edge)
+        else:
+            flag = False
+    agent0_tree = ArrayTree(agent0_branch_size, nodes, edges)
+    flag = True
+    edges = []
+    nodes = []
+    while(flag):
+        l = f.readline()
+        if l != "":
+            if l[0] == "(":
+                [edge, node] = get_node_and_edge(l)
+                nodes.append(node)
+                edges.append(edge)
+        else:
+            flag = False
+    agent1_tree = ArrayTree(agent1_branch_size, nodes, edges)
+    return [agent0_tree, agent1_tree]
+
