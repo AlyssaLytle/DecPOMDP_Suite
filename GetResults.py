6a87ebc63bb0430aa8a127838d38ffc566c292a1
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-05-25 09:37:14 -0400

    Added new path for results

--- GetResults.py
+++ GetResults.py
@@ -5,7 +5,7 @@ import csv
 #Call should look like python3 Getgit aResults.py filename start_state scenario_number
 [cmd, filename, start_state, scenario_number] = sys.argv
 result = "GMAA_" + filename + "_kGMAA_QMDP_h2_restarts1_k1_NoCache_BGIP-AM_AM_restarts10_JPol"
-path_to_res = "~/.madp/results/GMAA/" + filename + "/" + result
+path_to_res = "/afs/cs.unc.edu/home/abyrnes1/.madp/results/GMAA/" + filename + "/" + result
 
 with open('dpomdp.csv', newline='') as csvfile:
     dreader = csv.reader(csvfile)
