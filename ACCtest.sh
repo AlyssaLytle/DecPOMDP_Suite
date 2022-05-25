53ec291c5c6bee4fbaaffb374c205d4243336e6e
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-05-25 13:37:14 -0400

    Changed rewards to positive

--- ACCtest.sh
+++ ACCtest.sh
@@ -1,5 +1,15 @@
 timeout -k 5m 5m ../MADP/src/solvers/GMAA -G MAAstar -B BFS ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G FSPC -B BFS ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G kGMAA -B BFS ACC/ACC-ss-standby-scen-1.dpomdp -h2
 timeout -k 5m 5m ../MADP/src/solvers/GMAA -G MAAstar -B AM ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G FSPC -B AM ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G kGMAA -B AM ACC/ACC-ss-standby-scen-1.dpomdp -h2
 timeout -k 5m 5m ../MADP/src/solvers/GMAA -G MAAstar -B CE ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G FSPC -B CE ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G kGMAA -B CE ACC/ACC-ss-standby-scen-1.dpomdp -h2
 timeout -k 5m 5m ../MADP/src/solvers/GMAA -G MAAstar -B MP ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G FSPC -B MP ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G kGMAA -B MP ACC/ACC-ss-standby-scen-1.dpomdp -h2
 timeout -k 5m 5m ../MADP/src/solvers/GMAA -G MAAstar -B BnB ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G FSPC -B BnB ACC/ACC-ss-standby-scen-1.dpomdp -h2
+timeout -k 5m 5m ../MADP/src/solvers/GMAA -G kGMAA -B BnB ACC/ACC-ss-standby-scen-1.dpomdp -h2
