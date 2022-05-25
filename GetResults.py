8ac5e8b83510292ca0334f7417b922648eeec6c3
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-05-25 09:26:32 -0400

    Updated getresults to fix bugs

--- GetResults.py
+++ GetResults.py
@@ -2,22 +2,23 @@ import sys
 from DPOMDP_Writer.Reader import *
 from DPOMDP_Writer.DPOMDPWriterMedium import DPOMDPWriterACC
 import csv
-#Call should look like python3 GetResults.py filename start_state scenario_number
+#Call should look like python3 Getgit aResults.py filename start_state scenario_number
 [cmd, filename, start_state, scenario_number] = sys.argv
 result = "GMAA_" + filename + "_kGMAA_QMDP_h2_restarts1_k1_NoCache_BGIP-AM_AM_restarts10_JPol"
 path_to_res = "~/.madp/results/" + filename + "/" + result
 
 with open('dpomdp.csv', newline='') as csvfile:
     dreader = csv.reader(csvfile)
-    machine_comm_actions = list(next(dreader))
-    machine_mvmt_actions = list(next(dreader))
-    human_comm_actions = list(next(dreader))
-    human_mvmt_actions = list(next(dreader))
-    modes = list(next(dreader))
-    prob_dict = list(next(dreader))
-    cost_dict = list(next(dreader))
-    human_observations = list(next(dreader))
-    machine_observations = list(next(dreader))
+    data = list(dreader)
+    machine_comm_actions = data[0]
+    machine_mvmt_actions = data[1]
+    human_comm_actions = data[2]
+    human_mvmt_actions = data[3]
+    modes = data[4]
+    prob_dict = data[5]
+    cost_dict = data[6]
+    human_observations = data[7]
+    machine_observations = data[8]
 
 writer = DPOMDPWriterACC(machine_comm_actions, machine_mvmt_actions, human_comm_actions, human_mvmt_actions, modes,prob_dict,cost_dict,scenario_number, human_observations, machine_observations)
 
