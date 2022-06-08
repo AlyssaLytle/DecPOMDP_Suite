3e2906a3dea9bca3b2eacbe969721cfd96d557c2
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-08 10:58:31 -0400

    Updated test shell script to try two specific runs with no timeout

--- PrintResults.py
+++ PrintResults.py
@@ -1,5 +1,7 @@
 import os
 
+
+
 readfile = "lsOutput.log"
 f = open(readfile, "r")
 shell_commands = ""
@@ -7,8 +9,8 @@ for x in f:
     if x[-4:-1] == "Pol":
         file_path = "~/.madp/results/GMAA/ACC-ss-standby-scen-1/" + x[:-1]
         shell_commands += "cat " + file_path + "\n"
-            
 f = open("printres.sh", "w")
 f.writelines(shell_commands)
 f.close()
         
+
