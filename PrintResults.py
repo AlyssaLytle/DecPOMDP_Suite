5fcdb82f80fc48f3fc824ffa03b68c5e788076ba
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-08 10:44:58 -0400

    fixed syntax in shell scr

--- PrintResults.py
+++ PrintResults.py
@@ -4,10 +4,13 @@ readfile = "lsOutput.log"
 f = open(readfile, "r")
 shell_commands = ""
 for x in f:
-    print(x[-4:-1])
     if x[-4:-1] == "Pol":
         file_path = "~/.madp/results/GMAA/ACC-ss-standby-scen-1/" + x
         print(x)
-        #if os.path.getsize(file_path) != 0:
-        #    print()
+        if os.path.getsize(file_path) != 0:
+            shell_commands += "cat " + file_path + "\n"
+            
+f = open("printres.sh", "w")
+f.writelines(shell_commands)
+f.close()
         
