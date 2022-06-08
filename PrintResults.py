b034beebf23670335c21717deea023cd7891b38c
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-08 10:40:01 -0400

    fixed syntax in shell script

--- PrintResults.py
+++ PrintResults.py
@@ -4,7 +4,7 @@ readfile = "lsOutput.log"
 f = open(readfile, "r")
 shell_commands = ""
 for x in f:
-    print(x)
+    print(x[-3:])
     if x[-3:] == "Pol":
         file_path = "~/.madp/results/GMAA/ACC-ss-standby-scen-1/" + x
         #if os.path.getsize(file_path) != 0:
