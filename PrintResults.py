772e5f0dae0d0a6123fb4ed27b4ab866c930fdef
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-08 10:28:50 -0400

    change to print method :

--- PrintResults.py
+++ PrintResults.py
@@ -4,9 +4,9 @@ readfile = "lsOutput.log"
 f = open(readfile, "r")
 shell_commands = ""
 for x in f:
+    print(x)
     if x[-3:] == "Pol":
         file_path = "~/.madp/results/GMAA/ACC-ss-standby-scen-1/" + x
         if os.path.getsize(file_path) != 0:
             print(x)
         
-
