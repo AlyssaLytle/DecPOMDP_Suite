79d8b04d7eb8d279f19eb4496ac0ea0bed319da0
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-08 10:47:53 -0400

    fixed syntax issue

--- PrintResults.py
+++ PrintResults.py
@@ -6,9 +6,7 @@ shell_commands = ""
 for x in f:
     if x[-4:-1] == "Pol":
         file_path = "~/.madp/results/GMAA/ACC-ss-standby-scen-1/" + x[:-1]
-        print(x)
-        if os.path.getsize(file_path) != 0:
-            shell_commands += "cat " + file_path + "\n"
+        shell_commands += "cat " + file_path + "\n"
             
 f = open("printres.sh", "w")
 f.writelines(shell_commands)
