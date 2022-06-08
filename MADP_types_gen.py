6206f6de3b5a5d557305b1e84ab1226f76a9d9a6
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-08 07:55:16 -0400

    Changed timeout for test methods

--- MADP_types_gen.py
+++ MADP_types_gen.py
@@ -6,29 +6,34 @@ solver_types = ["BFS", "AM", "CE", "MP", "BnB"]
 GMAA_param = ["MAAstar", "FSPC", "kGMAA"]
 q_heur = ["QMDP", "QPOMDP", "QBG", "QMDPc", "QPOMDPav", "QBGav", "QHybrid", "QPOMDPhybrid", "QBGhybrid", "QBGTreeIncPrune", "QBGTreeIncPruneBnB"]
 
+count = 0
 
 output = ""
 for solver in solver_types:
     for gmaa_param in GMAA_param:
         for q in q_heur:
-            command = "timeout -k 5m 5m ../MADP/src/solvers/GMAA "
+            command = "timeout -k 1m 1m ../MADP/src/solvers/GMAA "
             command += "-G " + gmaa_param 
             command += " -B " + solver 
             command += " -Q " + q
             command += " MADPtools/ACC/ACC-ss-standby-scen-1.dpomdp -h2"
             output += command + "\n"
+            count += 1
         
 
 for solver in solver_types:
     for q in q_heur:
-        command = "timeout -k 5m 5m ../MADP/src/solvers/GMAA "
+        command = "timeout -k 1m 1m ../MADP/src/solvers/GMAA "
         command += "-G MAAstar"
         command += " -B " + solver 
         command += " -Q " + q
         command += " MADPtools/ACC/ACC-ss-standby-scen-1.dpomdp -h2"
         output += command + "\n"
+        count += 1
         
 f = open("ACCtest.sh", "w")
 f.writelines(output)
 f.close()
+
+print((54*1)/60)
 
