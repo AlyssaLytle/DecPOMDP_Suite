9a8aaea7d54ee72605807418cf5786f27f06e09d
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-12 23:02:06 -0400

    Fixed bug in reader

--- MADPtools/DPOMDP_Writer/Reader.py
+++ MADPtools/DPOMDP_Writer/Reader.py
@@ -19,35 +19,37 @@ def get_trees(filename, agent0_branch_size, agent1_branch_size):
     #skip first 3 lines
     if f.readline():
         print("line read")
-    f.readline()
-    f.readline()
-    flag = True
-    edges = []
-    nodes = []
-    while(flag):
-        l = f.readline()
-        if l:
-            if l[0] == "(":
-                [edge, node] = get_node_and_edge(l)
-                nodes.append(node)
-                edges.append(edge)
+        f.readline()
+        f.readline()
+        flag = True
+        edges = []
+        nodes = []
+        while(flag):
+            l = f.readline()
+            if l:
+                if l[0] == "(":
+                    [edge, node] = get_node_and_edge(l)
+                    nodes.append(node)
+                    edges.append(edge)
+                else:
+                    flag = False
             else:
                 flag = False
-        else:
-            flag = False
-    agent0_tree = ArrayTree(agent0_branch_size, nodes, edges)
-    flag = True
-    edges = []
-    nodes = []
-    while(flag):
-        l = f.readline()
-        if l:
-            if l[0] == "(":
-                [edge, node] = get_node_and_edge(l)
-                nodes.append(node)
-                edges.append(edge)
-        else:
-            flag = False
-    agent1_tree = ArrayTree(agent1_branch_size, nodes, edges)
-    return [agent0_tree, agent1_tree]
+        agent0_tree = ArrayTree(agent0_branch_size, nodes, edges)
+        flag = True
+        edges = []
+        nodes = []
+        while(flag):
+            l = f.readline()
+            if l:
+                if l[0] == "(":
+                    [edge, node] = get_node_and_edge(l)
+                    nodes.append(node)
+                    edges.append(edge)
+            else:
+                flag = False
+        agent1_tree = ArrayTree(agent1_branch_size, nodes, edges)
+        return [agent0_tree, agent1_tree]
+    else:
+        return []
 
