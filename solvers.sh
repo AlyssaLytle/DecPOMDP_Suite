89196d01e4f057248b6ef00437fcf8c4e73f3aa7
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-15 10:35:52 -0400

    Got rid of timeouts for shell script

--- solvers.sh
+++ solvers.sh
@@ -1,3 +1,3 @@
-timeout -k 30m 30m ../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2
-timeout -k 30m 30m ../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC/ACC-ss-standby-scen-1.dpomdp -h2
-timeout -k 30m 30m ../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC/ACC-ss-standby-scen-1-obs.dpomdp -h2
+../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2
+../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC/ACC-ss-standby-scen-1.dpomdp -h2
+../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC/ACC-ss-standby-scen-1-obs.dpomdp -h2
