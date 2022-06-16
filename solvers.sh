055a549d5aa3dc31f1be191a692b27c2534f77a9
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-15 23:58:43 -0400

    Updated shell script to output times before and after running each command

--- solvers.sh
+++ solvers.sh
@@ -1,3 +1,9 @@
+date +%T
 ../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2
+date +%T
 ../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC/ACC-ss-standby-scen-1.dpomdp -h2
+date +%T
 ../MADP/src/solvers/GMAA --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC/ACC-ss-standby-scen-1-obs.dpomdp -h2
+date +%T
+../MADP/src/solvers/GMAA MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2
+date +%T
