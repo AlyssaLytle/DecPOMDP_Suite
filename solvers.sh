d593291e6f611e43978148a28bb9d9d49d73c590
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-16 21:54:03 -0400

    Just running minimal example

--- solvers.sh
+++ solvers.sh
@@ -1,5 +1,5 @@
 date +%T
-../MADP/src/solvers/GMAA --verbose --verbose --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 > min_pol.txt
+../MADP/src/solvers/GMAA --verbose --verbose --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 
 date +%T
-../MADP/src/solvers/GMAA --verbose --verbose --sparse  MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 > min_pol2.txt
+../MADP/src/solvers/GMAA --verbose --verbose --sparse  MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 
 date +%T
