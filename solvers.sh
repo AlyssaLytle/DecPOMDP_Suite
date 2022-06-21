dc06f22017043dd9ff4cfc16298bc41460bd75c2
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-06-21 15:36:28 -0400

    Updated shell to only run one solver because it's crashing an not saving any output

--- solvers.sh
+++ solvers.sh
@@ -1,5 +1,3 @@
 date +%T
 ../MADP/src/solvers/GMAA --verbose --verbose --sparse --GMAA=MAAstar --BGIP_Solver=BnB --BnB-ordering=Prob MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 
 date +%T
-../MADP/src/solvers/GMAA --verbose --verbose --sparse  MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 
-date +%T
