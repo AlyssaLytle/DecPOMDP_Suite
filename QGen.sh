e0a085a2c5f92a58b64b0cd395af974ed2bbe686
Alyssa Byrnes <alyssa@Alyssas-MacBook-Pro.local>
2022-07-01 13:07:09 -0400

    Run subset of qgen

--- QGen.sh
+++ QGen.sh
@@ -9,27 +9,27 @@ date +%T
 #echo "Finding value for QBG"
 #timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBG > QBG.log
 #date +%T
-echo "Finding value for QMDPc"
-timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QMDPc > QMDPc.log
-date +%T
-echo "Finding value for QPOMDPav"
-timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QPOMDPav > QPOMDPav.log
-date +%T
-echo "Finding value for QBGav"
-timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBGav > QBGav.log
-date +%T
-echo "Finding value for QHybrid"
-timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QHybrid > QHybrid.log
-date +%T
-echo "Finding value for QPOMDPhybrid"
-timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QPOMDPhybrid > QPOMDPhybrid.log
-date +%T
-echo "Finding value for QBGhybrid"
-timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBGhybrid > QBGhybrid.log
-date +%T
-echo "Finding value for QBGTreeIncPrune"
-timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBGTreeIncPrune > QBGTreeIncPrune.log
-date +%T
-echo "Finding value for QBGTreeIncPruneBnB"
-timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBGTreeIncPruneBnB > QBGTreeIncPruneBnB.log
-date +%T
+# echo "Finding value for QMDPc"
+# timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QMDPc > QMDPc.log
+# date +%T
+# echo "Finding value for QPOMDPav"
+# timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QPOMDPav > QPOMDPav.log
+# date +%T
+# echo "Finding value for QBGav"
+# timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBGav > QBGav.log
+# date +%T
+# echo "Finding value for QHybrid"
+# timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QHybrid > QHybrid.log
+# date +%T
+# echo "Finding value for QPOMDPhybrid"
+# timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QPOMDPhybrid > QPOMDPhybrid.log
+# date +%T
+# echo "Finding value for QBGhybrid"
+# timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBGhybrid > QBGhybrid.log
+# date +%T
+# echo "Finding value for QBGTreeIncPrune"
+# timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBGTreeIncPrune > QBGTreeIncPrune.log
+# date +%T
+# echo "Finding value for QBGTreeIncPruneBnB"
+# timeout -k 1h 1h ./calculateQheuristic ~/public/alyssadpomdp/DecPOMDP_Suite/MADPtools/ACC-min/ACC-standby-s1.dpomdp -h2 -Q QBGTreeIncPruneBnB > QBGTreeIncPruneBnB.log
+# date +%T
