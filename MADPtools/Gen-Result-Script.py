bd63fe03a408ffc5c0d3bda310e673ab4467a48a
alyssabyrnes <alyssanbyrnes@gmail.com>
2022-09-03 10:17:33 -0400

    Updated results gen script

--- MADPtools/Gen-Result-Script.py
+++ MADPtools/Gen-Result-Script.py
@@ -7,16 +7,17 @@ def gen_result_make_file(prefix):
     else:
         modes = ["standby", "following", "speedcontrol", "error", "hold"]
     for start_state in modes:
-        by_start_mode_value_file = "results/tree_values_" + start_state + "-" + prefix + ".csv"
+        by_start_mode_value_file = "tree_values_" + start_state + "-" + prefix + ".csv"
         output += "touch " + by_start_mode_value_file + "\n"    
     for scenario_number in scenarios:
-        by_scen_value_file = "results/tree_values_s" + str(scenario_number) + "-" + prefix + ".csv"
+        by_scen_value_file = "tree_values_s" + str(scenario_number) + "-" + prefix + ".csv"
         output += "touch " + by_scen_value_file + "\n"  
     return output
         
 shell_name = "InitializeResultFiles.sh"
 output = "rm -r figs/*\n"
 output += "rm -r results/*\n"
+output += "cd results\n"
 output += gen_result_make_file("ACC")
 output += gen_result_make_file("ACC-min")
 f = open(shell_name, "w")
