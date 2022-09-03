dd993e7a4356f6f29b22a5b4ac074a1882b2cb4f
alyssabyrnes <alyssanbyrnes@gmail.com>
2022-09-03 10:15:18 -0400

    Added script to help initialize result files

---
+++ MADPtools/InitializeResultFiles.sh
@@ -0,0 +1,29 @@
+rm -r figs/*
+rm -r results/*
+touch results/tree_values_standby-ACC.csv
+touch results/tree_values_following-ACC.csv
+touch results/tree_values_speedcontrol-ACC.csv
+touch results/tree_values_error-ACC.csv
+touch results/tree_values_hold-ACC.csv
+touch results/tree_values_override-ACC.csv
+touch results/tree_values_s0-ACC.csv
+touch results/tree_values_s1-ACC.csv
+touch results/tree_values_s2-ACC.csv
+touch results/tree_values_s3-ACC.csv
+touch results/tree_values_s4-ACC.csv
+touch results/tree_values_s5-ACC.csv
+touch results/tree_values_s6-ACC.csv
+touch results/tree_values_s7-ACC.csv
+touch results/tree_values_standby-ACC-min.csv
+touch results/tree_values_following-ACC-min.csv
+touch results/tree_values_speedcontrol-ACC-min.csv
+touch results/tree_values_error-ACC-min.csv
+touch results/tree_values_hold-ACC-min.csv
+touch results/tree_values_s0-ACC-min.csv
+touch results/tree_values_s1-ACC-min.csv
+touch results/tree_values_s2-ACC-min.csv
+touch results/tree_values_s3-ACC-min.csv
+touch results/tree_values_s4-ACC-min.csv
+touch results/tree_values_s5-ACC-min.csv
+touch results/tree_values_s6-ACC-min.csv
+touch results/tree_values_s7-ACC-min.csv
