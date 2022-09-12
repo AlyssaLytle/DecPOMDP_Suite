ae71deb5d36deb442640e694766a83ddf8f42ee6
alyssabyrnes <alyssanbyrnes@gmail.com>
2022-09-11 20:51:26 -0400

    Fixed bugs in shell

--- MADPtools/InitializeResultFiles.sh
+++ MADPtools/InitializeResultFiles.sh
@@ -1,30 +1,3 @@
 rm -r figs/*
 rm -r results/*
 
-touch tree_values_standby-ACC.csv
-touch tree_values_following-ACC.csv
-touch tree_values_speedcontrol-ACC.csv
-touch tree_values_error-ACC.csv
-touch tree_values_hold-ACC.csv
-touch tree_values_override-ACC.csv
-touch tree_values_s0-ACC.csv
-touch tree_values_s1-ACC.csv
-touch tree_values_s2-ACC.csv
-touch tree_values_s3-ACC.csv
-touch tree_values_s4-ACC.csv
-touch tree_values_s5-ACC.csv
-touch tree_values_s6-ACC.csv
-touch tree_values_s7-ACC.csv
-touch tree_values_standby-ACC-min.csv
-touch tree_values_following-ACC-min.csv
-touch tree_values_speedcontrol-ACC-min.csv
-touch tree_values_error-ACC-min.csv
-touch tree_values_hold-ACC-min.csv
-touch tree_values_s0-ACC-min.csv
-touch tree_values_s1-ACC-min.csv
-touch tree_values_s2-ACC-min.csv
-touch tree_values_s3-ACC-min.csv
-touch tree_values_s4-ACC-min.csv
-touch tree_values_s5-ACC-min.csv
-touch tree_values_s6-ACC-min.csv
-touch tree_values_s7-ACC-min.csv
